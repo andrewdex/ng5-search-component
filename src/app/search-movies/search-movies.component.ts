e389a1f7f8d00e6c4d757e82f0492e8276258a0d
Dilusha Gonagala <andrewdexis@gmail.com>
2018-02-18 00:42:29 +0800

    added tabbing with keyboard down arrow

--- src/app/search-movies/search-movies.component.ts
+++ src/app/search-movies/search-movies.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
 import { MoviesService } from '../services/movies.service';
-
+import * as $ from "jquery";
 @Component({
   selector: 'app-search-movies',
   templateUrl: './search-movies.component.html',
@@ -16,7 +15,8 @@ export class SearchMoviesComponent implements OnInit {
   autoCompleteEnableWrapper =true;
   title:String;
   keypressTrack : number = 0;
-
+  noresults = false;
+  tabIndex=0;
   constructor(private moviesService:MoviesService) {
 
     this.movies =  ['Atlantic','Anna Belle','Burnt','Captain America','Dawn of Planet Apes','Edge of Tomorrow','Fast and Furius','Goosebumps','Hobbit','Inception','John Wick','Koob','King Kong','Lion King','Man of Steel', 'Nice at the Meusium','Once Upon A Time','Perl habour','Quantum','Ragnarok','Shout out'];
@@ -29,6 +29,32 @@ export class SearchMoviesComponent implements OnInit {
     console.log(data);
   });
 
+  let displayBoxIndex = -1;
+
+    $("#search").keyup(function(e)  
+    {
+        if (e.keyCode == 40) 
+        {  
+            Navigate(1);
+        }
+        if(e.keyCode==38)
+        {
+            Navigate(-1);
+        }
+      
+    });
+
+  var Navigate = function(diff) {
+    displayBoxIndex += diff;
+    var oBoxCollection = $(".movie");
+    if (displayBoxIndex >= oBoxCollection.length)
+        displayBoxIndex = 0;
+    if (displayBoxIndex < 0)
+        displayBoxIndex = oBoxCollection.length - 1;
+    var cssClass = "selected";
+    oBoxCollection.removeClass(cssClass).eq(displayBoxIndex).addClass(cssClass);
+  }
+    
   
   }
 
@@ -56,12 +82,19 @@ export class SearchMoviesComponent implements OnInit {
       if($event.timeStamp - this.keypressTrack > 500 ){
         if(sanitizedText !== ""){
           this.autoCompleteEnableWrapper = true;
+          this.noresults = false;
           console.log("SearchQuery :"+sanitizedText);
           console.log(sanitizedText);
           this.moviesService.searchMovieByTitle(sanitizedText).subscribe(data =>{
             console.log("hey");
             this.omdbMovies = data.Search;
+            
             console.log(data);
+
+            if(data.Error == "Movie not found!"){
+              this.autoCompleteEnableWrapper = false;
+              this.noresults = true;
+            }
           });
     
         }else{
@@ -72,6 +105,45 @@ export class SearchMoviesComponent implements OnInit {
 
 
 
+
+  }
+
+
+  onDown($event){
+  let tempIndex =0;
+    if(this.tabIndex > 0){
+      var previousValue = this.tabIndex -1 ;
+      var previousElement = document.querySelector("[tabindex='"+previousValue+"']");
+  
+      previousElement.classList.remove("selected");
+    }
+  
+    console.log("clicked");
+    console.log($event);
+    if($event.key == "ArrowDown"){
+      let selectedElement = document.querySelector("[tabindex='"+this.tabIndex+"']");
+      selectedElement.className = "selected";   
+       this.tabIndex ++;
+
+    }
+
+
+    if($event.key == "ArrowUp"){
+      let selectedElement = document.querySelector("[tabindex='"+this.tabIndex+"']");
+      selectedElement.className = "selected";   
+       this.tabIndex --;
+
+    }
+
+    if($event.key == "Enter"){
+
+      let selectedElement = document.querySelector("[tabindex='"+this.tabIndex+"']");
+      let getValue = selectedElement.querySelector(".title");
+      this.searchQuery = getValue.innerHTML;
+      console.log(getValue.innerHTML);
+
+    }
+ 
 
   }
 
