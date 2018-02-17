0ac718df4d1118075407e4a55cb4cfc05d839da2
Dilusha Gonagala <andrewdexis@gmail.com>
2018-02-17 20:00:35 +0800

    init movie search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SearchMoviesComponent } from './search-movies/search-movies.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchMoviesComponent
   ],
   imports: [
     BrowserModule
